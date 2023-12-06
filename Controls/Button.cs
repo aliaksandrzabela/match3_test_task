8874444aaa70f283571cd85d48dd53028c8096dc
Забелло Александр <aliaksandr.zabela.gamefactory@gmail.com>
2023-12-06 08:12:55 +0300

    feature: InitProject, Button component

---
+++ Controls/Button.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace monogame_match3.Controls
+{
+    public class Button : Component
+    {        
+        public event EventHandler Click;
+        public Vector2 Position { get; set; }
+        public Rectangle Rectangle
+        {
+            get
+            {
+                return new Rectangle((int)Position.X, (int)Position.Y, texture.Width, texture.Height);
+            }
+        }
+
+        public string Text { get; set; }
+        public Color PenColor { get; set; }
+
+        public Button(Texture2D texture, SpriteFont font)
+        {
+            this.texture = texture;
+            this.font = font;
+        }
+
+        private MouseState mouseState;
+        private MouseState previousMouseState;
+        private SpriteFont font;
+        private bool isMouseOver;
+
+        private Texture2D texture;        
+
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, Rectangle, isMouseOver ? Color.Gray : Color.White);
+
+            if(!string.IsNullOrEmpty(Text))
+            {
+                float x = (Rectangle.X + (Rectangle.Width / 2)) - (font.MeasureString(Text).X / 2);
+                float y = (Rectangle.Y + (Rectangle.Height / 2)) - (font.MeasureString(Text).Y / 2);
+
+                spriteBatch.DrawString(font, Text, new Vector2(x, y), PenColor);
+            }
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            previousMouseState = mouseState;
+            mouseState = Mouse.GetState();
+
+            Rectangle mouseRectangle = new Rectangle(mouseState.X, mouseState.Y, 1, 1);
+
+            isMouseOver = false;
+
+            if(mouseRectangle.Intersects(Rectangle))
+            {
+                if(mouseState.LeftButton == ButtonState.Released && previousMouseState.LeftButton == ButtonState.Pressed)
+                {
+                    Click?.Invoke(this, EventArgs.Empty);
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new monogame_match3.Match3Game();
+game.Run();
